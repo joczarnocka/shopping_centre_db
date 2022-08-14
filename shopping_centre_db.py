bcd0431c3a71bba27805ac9ecf6ad5f67455ff11
joczarnocka <42293202+joczarnocka@users.noreply.github.com>
2022-08-14 19:48:23 +0200

    add database CRUD functions

--- shopping_centre_db.py
+++ shopping_centre_db.py
@@ -1,2 +1,163 @@
 import sqlite3
 from sqlite3 import Error
+
+
+def create_connection(db_file):
+   """ create a database connection to the SQLite database
+       specified by db_file
+   :param db_file: database file
+   :return: Connection object or None
+   """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       return conn
+   except Error as e:
+       print(e)
+
+   return conn
+
+def execute_sql(conn, sql):
+   """ Execute sql
+   :param conn: Connection object
+   :param sql: a SQL script
+   :return:
+   """
+   try:
+       c = conn.cursor()
+       c.execute(sql)
+   except Error as e:
+       print(e)
+
+def add_shop(conn, shop):
+   """
+   Create a new shop into the shops table
+   :param conn:
+   :param shop:
+   :return: shop id
+   """
+   sql = '''INSERT INTO shops(name, specialization, address)
+             VALUES(?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, shop)
+   conn.commit()
+   return cur.lastrowid
+
+
+def add_product(conn, product):
+   """
+   Create a new product into the products table
+   :param conn:
+   :param task:
+   :return: task id
+   """
+   sql = '''INSERT INTO products(shop_id, name, description, number, price)
+             VALUES(?,?,?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, product)
+   conn.commit()
+   return cur.lastrowid
+
+
+def select_product_by_name(conn, name):
+   """
+   Query products by name
+   :param conn: the Connection object
+   :param name:
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM products WHERE name=?", (name,))
+
+   rows = cur.fetchall()
+   return rows
+
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows  
+
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
+def delete_where(conn, table, **kwargs):
+   """
+   Delete from table where attributes from
+   :param conn:  Connection to the SQLite database
+   :param table: table name
+   :param kwargs: dict of attributes and values
+   :return:
+   """
+   qs = []
+   values = tuple()
+   for k, v in kwargs.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+
+   sql = f'DELETE FROM {table} WHERE {q}'
+   cur = conn.cursor()
+   cur.execute(sql, values)
+   conn.commit()
+   print("Deleted")
+
+def delete_all(conn, table):
+   """
+   Delete all rows from table
+   :param conn: Connection to the SQLite database
+   :param table: table name
+   :return:
+   """
+   sql = f'DELETE FROM {table}'
+   cur = conn.cursor()
+   cur.execute(sql)
+   conn.commit()
+   print("Deleted")
